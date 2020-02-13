239fbe8ca775ef75b7657c34835983eca5e07b0a
a93-git <42908045+a93-git@users.noreply.github.com>
2020-02-13 17:05:30 +0530

    Add error handling and move files

--- service_restart.ps1
+++ service_restart.ps1
@@ -1,35 +1,52 @@
-# 1. Stop the service
-Stop-Service -Force -Name "PerfHost"
+$Service = 'MSSQLSERVER'
+$CacheFolderName = 'test folder 2'
 
-# 1(a) Check if the service has been stopped
+# Stop the service
+Get-Service -Name $Service | Stop-Service -Force -WarningAction SilentlyContinue
 
-$service_status = Get-Service "PerfHost" | Select-Object Status -ExpandProperty Status
+# Get current service status
+$ServiceStatus = Get-Service $Service | Select-Object Status -ExpandProperty Status
 
-While (1) {
-	if ( $service_status -eq "Stopped" ) {
+# Wait for the service to stop
+$Count = 0
+While($Count -ne 60) {
+    if ( $ServiceStatus -eq "Stopped" ) {
         Write-Host "Service has been stopped"
-		break
-	} else {
-		sleep(60)
-	}
+        break
+    } else {
+        Start-Sleep(60)
+        $Count = $Count + 1
+    }
 }
-	
-# 2. Clear the cache
-Get-ChildItem -Path "C:\Users\Administrator\Documents\files_to_delete\*" | Remove-Item
 
-# 3. Start the service
+# Backup of the cache
+$Date = Get-Date -format dd-MM-yyyy
+$CopyFromPath = "C:\Users\Administrator\Documents\test folder\test folder 2"
+$CopyToPath = $CopyFromPath + $Date
 
-Start-Service -Name "PerfHost"
+Try {
+    $BackupDirParent = Split-Path $CopyFromPath
+    New-Item -ItemType 'directory' -Path $BackupDirParent -Name "$CacheFolderName$Date" -ErrorAction Stop | Out-Null
+    Move-Item -Path "$CopyFromPath\*" -Destination "$CopyToPath\" -Force -InformationAction SilentlyContinue -ErrorAction Stop
+#    $MoveAttempt = $True
+}
+Catch {
+    "Unable to move the files"
+#    $MoveAttempt = $False
+}
 
-# 3(a) Check if the service is running
-$service_status = Get-Service "PerfHost" | Select-Object Status -ExpandProperty Status
-$count = 0
-While ($count -ne 45) {
-	if ( $service_status -eq "Running" ) {
+# Start the service
+Start-Service -Name $Service
+
+# Confirm if the service is running
+$ServiceStatus = Get-Service $Service | Select-Object Status -ExpandProperty Status
+$Count = 0
+While ($Count -ne 60) {
+	if ( $ServiceStatus -eq "Running" ) {
         Write-Host "Service has been started"
 	    break
 	} else {
-		sleep(60)
-		$count = $count + 1
+		Start-Sleep(60)
+		$Count = $Count + 1
 	}
 }
