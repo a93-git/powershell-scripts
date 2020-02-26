b294f02616b02645ff26308fbbfce73b0a2ff114
a93-git <42908045+a93-git@users.noreply.github.com>
2020-02-26 21:20:55 +0530

    Create ADAudit.ps1

---
+++ ADAudit.ps1
@@ -0,0 +1,9 @@
+# Get the 
+
+
+Get-ADUser -Filter * | Select-Object SamAccountName | Out-File ".\users.txt"
+Get-Content ".\users.txt" | ForEach-Object -Process {$_.Replace(' ', '')} | Out-File ".\users2.txt"
+Get-Content -Path ".\users2.txt" | 
+    ForEach-Object -Process { Get-ADUser $_ -Properties SamAccountName,UserPrincipalName, DisplayName, Enabled, PasswordExpired, DistinguishedName, AccountLockoutTime, AccountExpirationDate, BadLogonCount, badPasswordTime, badPwdCount, CannotChangePassword, CanonicalName, Deleted, DoesNotRequirePreAuth, EmailAddress, isDeleted, LastBadPasswordAttempt, lastLogoff, lastLogon, LastLogonDate, lastLogonTimestamp, LockedOut, lockoutTime, logonCount, LogonWorkstations, PasswordLastSet, PasswordNeverExpires, PasswordNotRequired, ProtectedFromAccidentalDeletion, SID, pwdLastSet, TrustedForDelegation, TrustedToAuthForDelegation} | 
+    Select-Object SamAccountName,UserPrincipalName, DisplayName, Enabled, PasswordExpired, DistinguishedName, AccountLockoutTime, AccountExpirationDate, BadLogonCount, badPasswordTime, badPwdCount, CannotChangePassword, CanonicalName, Deleted, DoesNotRequirePreAuth, EmailAddress, isDeleted, LastBadPasswordAttempt, lastLogoff, lastLogon, LastLogonDate, lastLogonTimestamp, LockedOut, lockoutTime, logonCount, LogonWorkstations, PasswordLastSet, PasswordNeverExpires, PasswordNotRequired, ProtectedFromAccidentalDeletion, SID, pwdLastSet, TrustedForDelegation, TrustedToAuthForDelegation | 
+    Export-Csv -Path ".\ADAudit.csv" -NoTypeInformation
